a69efb94ee68da5228ea29e47078e9affce6c060
sunyong <sunyongmofang@hotmail.com>
2017-01-04 18:40:01 +0800

    update worker power

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-yum -y update
 yum -y install vim git gcc pcre-devel openssl-devel asciidoc xmlto man
 
 git config --global user.name "sunyong"
@@ -16,5 +15,11 @@ make && make install
 chkconfig iptables off
 service iptables stop
 
-echo "please run command:"
+cd > /dev/null
+
+su - worker
+git config --global user.name "sunyong"
+git config --global user.email sunyongmofang@163.com
+
+echo "please run command at worker:"
 echo "ss-server -s 0.0.0.0 -p 8388 -l 1080 -k password -m aes-256-cfb -t 600 -u"
